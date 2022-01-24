b432a9175777309c449855a466ab07b98a3cd44d
oldghoast <2281901186@qq.com>
2022-01-24 17:57:13 +0800

    20220124

---
+++ base/src/main/java/HashCodeTest.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName HashCodeTest
+ * @Description TODO
+ * @Author oldghoast
+ * @date 2022/1/24 10:00
+ * @Version 1.0
+ */
+public class HashCodeTest {
+
+    public static void main(String[] args) {
+        System.out.println("abc".hashCode());
+    }
+
+}
