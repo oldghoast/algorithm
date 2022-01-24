5dc115d9ecbce1d4a667a158e2dd059c51f6e5ea
oldghoast <2281901186@qq.com>
2022-01-24 22:54:13 +0800

    20220124

--- base/src/main/java/HashCodeTest.java
+++ base/src/main/java/HashCodeTest.java
@@ -8,7 +8,7 @@
 public class HashCodeTest {
 
     public static void main(String[] args) {
-        System.out.println("abc".hashCode());
+        //System.out.println("abc".hashCode());
     }
 
 }
